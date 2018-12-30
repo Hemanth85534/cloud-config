6b11d20ea5329f9c6537783f86d0ba3b5d0a4383
Hemanth <Hemanth@DESKTOP-04B06CK>
2018-12-31 00:05:16 +0530

    hello

---
+++ limitwithgit/src/test/java/com/java/limit/limitwithgit/LimitwithgitApplicationTests.java
@@ -0,0 +1,17 @@
+package com.java.limit.limitwithgit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LimitwithgitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
